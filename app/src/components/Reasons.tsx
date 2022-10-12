44f045ff0597fcc31ea745702ae4f13bc6774734
Noah Loomis <nloomis@spiria.com>
2022-10-12 14:04:03 -0400

    Okay done now

--- app/src/components/Reasons.tsx
+++ app/src/components/Reasons.tsx
@@ -15,19 +15,16 @@ const Reasons = () => {
     return (
       <div className="content-area">
         <div className="grid grid-cols-2 gap-2">
-          {[1, 2, 3, 4, 5, 6 ].map((num) => (
+          {[1, 2, 3, 4, 5, 6].map((num) => (
             <div
               key={num}
-              className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto my-2"
+              className="border border-blue-300 shadow rounded-md p-4 w-full mx-auto my-2"
             >
               <div className="animate-pulse flex space-x-4">
                 <div className="flex-1 space-y-6 py-1">
                   <div className="h-2 bg-slate-700 rounded"></div>
                   <div className="space-y-3">
-                    <div className="grid grid-cols-3 gap-4">
-                      <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                      <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    </div>
+                    <div className="h-2 bg-slate-700 rounded "></div>
                     <div className="h-2 bg-slate-700 rounded"></div>
                   </div>
                 </div>
